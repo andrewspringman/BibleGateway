a22ae41207456c83b2ca4e687e9cfc30c59ea035
Andrew Springman <awspringman@gmail.com>
2017-09-29 21:48:51 -0400

    test changing version

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
-<h1> EXAMPLE USAGE </h1>
-<?php include ('BibleGateway.class.php');
-$bible = new BibleGateway('KJV');
-$bible->searchPassage('John 3:1-17');
-echo $bible->text;
-$bible->reference = 'John 1:1';
-echo '<br>';
-echo $bible->reference;
-echo $bible->permalink;
-echo $bible->text;
-echo '<br>';
-$bible->getVerseOfTheDay();
-echo $bible->text;
-echo $bible->permalink;
+<h1> Moravian Daily Texts</h1>
+<?php include ('BibleGateway.class.php');?>
+<style>
+.versenum {
+  display: none;
+}
+.chapternum {
+  display: none;
+}
+</style>
+<?php
+$bible = new BibleGateway('MSG');
+$verses = array(
+  'Psalm 112',
+  'Ezekiel 1,2',
+  'Hebrews 9:23–10:4',
+  'Amos 3:6',
+  'Romans 14:8'
+);
+foreach ($verses as $v) {
+  echo "<hr/>";
+  echo "<h3>$v</h3>";
+  $bible->searchPassage($v);
+  echo $bible->text;
+}
